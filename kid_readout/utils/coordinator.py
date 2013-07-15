475cd42ad2d97cb4beb0ab8d2ee202dc0e24e17e
ckjellstrand <cbk2120@columbia.edu>
2013-07-15 14:12:36 -0400

    Changed custom function from create_data_products_udp to create_data_products_experimental.

--- kid_readout/utils/coordinator.py
+++ kid_readout/utils/coordinator.py
@@ -18,7 +18,7 @@ class Coordinator(single_pixel.SinglePixelBaseband):
         self.aggregator = aggregator.Aggregator(parent=self, writer=self.writer)
         # self.catcher = catcher.KatcpCatcher(proc_func=self.aggregator.proc_raw_data, bufname=self.bufname, roachip=roachip)
         # self.catcher = catcher.UDPCatcher(publish_func=self.aggregator.create_data_products_udp, bufname=self.bufname, roachip=roachip)
-        self.catcher = catcher.PacketCatcher(publish_func=self.aggregator.create_data_products_udp, bufname=self.bufname, roachip=roachip)
+        self.catcher = catcher.PacketCatcher(publish_func=self.aggregator.create_data_products_experimental, bufname=self.bufname, roachip=roachip)
         
     def start_data_thread(self):
         self.catcher.start_data_thread()
