efdcbeae0e3b293dd8d7fe73963315ef667b1aba
gitj <glenn.caltech@gmail.com>
2013-11-03 16:28:07 -0500

    Add sweep_index to data block to make it easier to track fine sweeps around resonators

--- kid_readout/utils/data_block.py
+++ kid_readout/utils/data_block.py
@@ -9,7 +9,8 @@ lpf = scipy.signal.firwin(256,1/256.)
 
 class DataBlock():
     def __init__(self, data, tone, fftbin, 
-                     nsamp, nfft, t0 = 0, fs = 512e6):
+                     nsamp, nfft, t0 = 0, fs = 512e6,
+                     sweep_index = 0):
         self.data = data
         self.tone = tone
         self.fftbin = fftbin
@@ -18,6 +19,7 @@ class DataBlock():
         self.dt = 1/(fs/nfft)
         self.fs = fs
         self.t0 = t0
+        self.sweep_index = sweep_index
         self._mean = None
         self._lpf_data = None
         
@@ -32,15 +34,26 @@ class SweepData():
         self.sweep_id = sweep_id
         self.blocks = []
         self._freqs = []
+        self._sweep_indexes = []
     def add_block(self,block):
         f = (block.fs*block.tone)/block.nsamp
         idx = bisect.bisect(self._freqs, f)
         self._freqs.insert(idx,f)
+        self._sweep_indexes.insert(idx,block.sweep_index)
         self.blocks.insert(idx,block)
         
+    def select_index(self,index):
+        msk = self.sweep_indexes == index
+        freqs = self.freqs[msk]
+        data = self.data[msk]
+        return freqs,data
+    
     @property
     def freqs(self):
         return np.array(self._freqs)
     @property
     def data(self):
         return np.array([x.mean() for x in self.blocks])
+    @property
+    def sweep_indexes(self):
+        return np.array(self._sweep_indexes)
