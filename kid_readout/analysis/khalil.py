7a1a2f49d7e8cdf8bb807458225132fdbee50f18
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2013-11-05 11:11:13 -0500

    First commit of fitting code in resonator.py and khalil.py

---
+++ kid_readout/analysis/khalil.py
@@ -0,0 +1,64 @@
+"""
+This module uses models from the Khalil paper.
+"""
+
+from __future__ import division
+
+import numpy as np
+from lmfit import Parameters
+
+def cable_delay(f, delay, phi):
+    """
+    This assumes that signals go as exp(i \omega t) so that a time
+    delay corresponds to negative phase.
+    """
+    return np.exp(-1j * (2 * np.pi * f * delay + phi))
+
+def s21_11(params, f):
+    """
+    This is Equation 11, except that the parameter A is a complex
+    prefactor intended to encapsulate the 1 + \hat{\epsilon} as well
+    as any external gains and phase shifts.
+    """
+    A = (params['A_mag'].value *
+         np.exp(1j * params['A_phase'].value))
+    f_0 = params['f_0'].value
+    Q = params['Q'].value
+    Q_e = (params['Q_e_real'].value +
+           1j * params['Q_e_imag'].value)
+    return A * (1 - (Q * Q_e**-1 /
+                     (1 + 2j * Q * (f - f_0) / f_0)))
+
+def s21_11_delayed(params, f):
+    """
+    This adds a cable delay controlled by two parameters to the
+    generic model above.
+    """
+    delay = params['delay'].value
+    phi = params['phi'].value
+    return cable_delay(f, delay, phi) * s21_11(params, f)
+
+def guess_11(f, data):
+    """
+    Right now these Q values are magic numbers. I suppose the
+    design values are a good initial guess, but there might be a
+    good way to approximate them without doing the full fit.
+    """
+    p = Parameters()
+    p.add('f_0', value=f[np.argmin(abs(data))], min=0)
+    p.add('A_mag', value=np.mean((np.abs(data[0]), np.abs(data[-1]))), min=0)
+    p.add('A_phase', value=np.mean(np.angle(data)), min=-np.pi, max=np.pi)
+    p.add('Q', value=5e4, min=0)
+    p.add('Q_e_real', value=5e4)
+    p.add('Q_e_imag', value=0)
+    return p
+
+def Q_i(params):
+    """
+    Return the internal Q of the resonator.
+    """
+    Q = params['Q'].value
+    Q_e = (params['Q_e_real'].value +
+           1j * params['Q_e_imag'].value)
+    return (Q**-1 - np.real(Q_e**-1))**-1
+
---
+++ kid_readout/analysis/resonator.py
@@ -0,0 +1,83 @@
+from __future__ import division
+
+import numpy as np
+import matplotlib.pyplot as plt
+from lmfit import minimize
+
+class Resonator(object):
+    """
+    This class represents a single resonator.
+
+    The idea is that all of the model-dependent behavior is contained
+    in functions that are supplied to the class. There is a little bit
+    of Python magic that allows for easy access to the fit parameters
+    and functions of only the fit parameters.
+    """
+
+    def __init__(self, model, functions):
+        """
+        Parameter model is a function S_21(params, f)
+        Parameter functions is a dictionary that maps keys that are
+        valid Python variables to functions that take a Parameters
+        object as their only argument.
+        """
+        self.model = model
+        self._functions = functions
+
+    def __getattr__(self, attr):
+        """
+        Return a fit parameter or value derived from the fit
+        parameters. This allows syntax like r.Q_i after a fit has been
+        performed.
+        """
+        try:
+            return self.result.params[attr].value
+        except KeyError:
+            pass
+        try:
+            return self._functions[attr](self.result.params)
+        except KeyError:
+            raise AttributeError("'{}' object has no attribute '{}'".format(self.__class__.__name__, attr))
+
+    def __dir__(self):
+        return (dir(super(Resonator, self)) +
+                self.result.params.keys() +
+                self._functions.keys())
+    
+    def fit(self, f, data, initial):
+        """
+        Fit the S_21 data using the model given at instantiation.
+        Parameter initial is a Parameters object containing initial
+        values. It is modified by lmfit.
+        """
+        self.f = f
+        self.data = data
+        self.result = minimize(self.residual,
+                               initial,
+                               args=(f, data))
+                               
+    def residual(self, params, f, data):
+        """
+        This is the residual function used in by lmfit.
+        """
+        return np.abs(data - self.model(params, f))
+
+    def plot(self):
+        """
+        Plot the data, fit, and f_0.
+        """
+        model = self.model(self.result.params, self.f)
+        model_0 = model[np.argmin(abs(self.f_0 - self.f))]
+        interactive = plt.isinteractive()
+        plt.ioff()
+        fig = plt.figure()
+        plt.plot(self.f, 20*np.log10(abs(self.data)), '.b', label='data')
+        plt.plot(self.f, 20*np.log10(abs(model)), '-g', label='fit')
+        plt.plot(self.f_0, 20*np.log10(abs(model_0)), '.r', label='f_0')
+        plt.xlabel('frequency [Hz]')
+        plt.ylabel('|S_21| [dB]')
+        plt.legend(loc='lower right')
+        if interactive:
+            plt.ion()
+            plt.show()
+        return fig
