af529ee953a5e944f802fc33fe76b3de17748cc3
gitj <glenn.caltech@gmail.com>
2013-12-22 22:06:01 -0500

    more simple scripts for common tasks

---
+++ apps/monitor_led.py
@@ -0,0 +1,53 @@
+import numpy as np
+import time
+
+from kid_readout.utils import roach_interface, sweeps, data_file, data_block
+
+
+
+ri = roach_interface.RoachBaseband()
+#ri.initialize()
+ri.set_fft_gain(4)
+df = data_file.DataFile()
+
+f0s = np.load('/home/gjones/workspace/apps/first_pass_sc3x3_0813f9.npy')[:4]
+f0s.sort()
+#ri.set_tone_freqs(center_freqs, nsamp=2**20)
+ri._sync()
+
+ri.set_tone_freqs(f0s,nsamp=2**22)
+ri.set_dac_attenuator(50)
+
+df.log_hw_state(ri)
+df.log_adc_snap(ri)
+ri._sync()
+time.sleep(0.2)
+
+tsg = None
+t0 = time.time()
+dmod,addr = ri.get_data(128)
+chids = ri.fpga_fft_readout_indexes+1
+tones = ri.tone_bins[ri.readout_selection]
+nsamp = ri.tone_nsamp
+print "saving data"
+for m in range(len(chids)):
+    block = data_block.DataBlock(data = dmod[:,m], tone=tones[m], fftbin = chids[m], 
+             nsamp = nsamp, nfft = ri.nfft, wavenorm = ri.wavenorm, t0 = t0, fs = ri.fs)
+    tsg = df.add_block_to_timestream(block,tsg=tsg)
+
+ri.set_tone_freqs(f0s+100.0/1e6,nsamp=2**22)
+ri._sync()
+time.sleep(0.2)
+tsg = None
+dmod,addr = ri.get_data(128)
+chids = ri.fpga_fft_readout_indexes+1
+tones = ri.tone_bins[ri.readout_selection]
+nsamp = ri.tone_nsamp
+print "saving data"
+for m in range(len(chids)):
+    block = data_block.DataBlock(data = dmod[:,m], tone=tones[m], fftbin = chids[m], 
+             nsamp = nsamp, nfft = ri.nfft, wavenorm = ri.wavenorm, t0 = time.time(), fs = ri.fs)
+    tsg = df.add_block_to_timestream(block,tsg=tsg)
+
+df.close()
+    
---
+++ apps/monitor_tc.py
@@ -0,0 +1,40 @@
+import numpy as np
+import time
+
+from kid_readout.utils import roach_interface, sweeps, data_file, data_block
+
+
+
+ri = roach_interface.RoachBaseband()
+#ri.initialize()
+df = data_file.DataFile()
+
+f0s = np.load('/home/gjones/workspace/apps/first_pass_sc3x3_0813f9.npy')
+f0s.sort()
+#ri.set_tone_freqs(center_freqs, nsamp=2**20)
+ri._sync()
+
+ri.set_tone_freqs(np.array([f0s[0]]),nsamp=2**20)
+ri.set_dac_attenuator(47)
+
+df.log_hw_state(ri)
+df.log_adc_snap(ri)
+
+
+tsg = None
+try:
+    for m in range(60*6):
+        dmod,addr = ri.get_data(2)
+        chids = ri.fpga_fft_readout_indexes+1
+        tones = ri.tone_bins[ri.readout_selection]
+        nsamp = ri.tone_nsamp
+        print "saving data"
+        m = 0
+        block = data_block.DataBlock(data = dmod[:,m], tone=tones[m], fftbin = chids[m], 
+                 nsamp = nsamp, nfft = ri.nfft, wavenorm = ri.wavenorm, t0 = time.time(), fs = ri.fs)
+        tsg = df.add_block_to_timestream(block,tsg=tsg)
+        time.sleep(10)
+except:
+    pass
+df.close()
+    
