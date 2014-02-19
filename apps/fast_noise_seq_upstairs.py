4efd349b634ec9eb3c6e78a994ec3aac9ea1f357
gitj <glenn.caltech@gmail.com>
2014-02-19 11:15:24 -0500

    noise scripts for upstairs

--- apps/fast_noise_seq_upstairs.py
+++ apps/fast_noise_seq_upstairs.py
@@ -16,7 +16,7 @@ ri.set_dac_attenuator(19.5)
 
 #f0s = np.load('/home/gjones/workspace/apps/f8_fit_resonances.npy')
 #f0s = np.load('/home/gjones/workspace/apps/first_pass_sc3x3_0813f9.npy')
-f0s = np.load('/home/gjones/workspace/apps/sc5x4_0813f10_first_pass.npy')#[:4]
+f0s = np.load('/home/gjones/workspace/apps/sc5x4_0813f10_first_pass.npy')[:4]
 #f0s = np.load('/home/gjones/workspace/readout/apps/sc3x3_0813f9_2014-02-11.npy')
 f0s.sort()
 #f0s = f0s*0.99
@@ -41,7 +41,7 @@ print "loaded waveforms in", (time.time()-start),"seconds"
 sys.stdout.flush()
 time.sleep(1)
 
-atten_list = np.linspace(5,16,4)#[30]#[35.5,33.5,46.5,43.5,40.5,37.5]
+atten_list = [30]#[35.5,33.5,46.5,43.5,40.5,37.5]
 for atten in atten_list:
     df = data_file.DataFile()
     ri.set_dac_attenuator(atten)
@@ -74,24 +74,30 @@ for atten in atten_list:
     time.sleep(0.5)
     
     nsets = len(meas_cfs)/atonce
-    tsg = None
-    for iset in range(nsets):
-        selection = range(len(meas_cfs))[iset::nsets]
-        ri.select_fft_bins(selection)
-        ri._sync()
-        time.sleep(0.2)
-        dmod,addr = ri.get_data(1024,demod=True)
-        chids = ri.fpga_fft_readout_indexes+1
-        tones = ri.tone_bins[ri.bank,ri.readout_selection]
-        nsamp = ri.tone_nsamp
-        print "saving data"
-        for m in range(len(chids)):
-            print m
-            block = data_block.DataBlock(data = dmod[:,m], tone=tones[m], fftbin = chids[m], 
-                     nsamp = nsamp, nfft = ri.nfft, wavenorm = ri.wavenorm, t0 = time.time(), fs = ri.fs)
-            tsg = df.add_block_to_timestream(block, tsg=tsg)
-    df.sync()
+    start_time = time.time()
+    while time.time() - start_time < 20*60:
+        tsg = None
+        for iset in range(nsets):
+            selection = range(len(meas_cfs))[iset::nsets]
+            ri.select_fft_bins(selection)
+            ri._sync()
+            time.sleep(0.2)
+            dmod,addr = ri.get_data(128,demod=True)
+            chids = ri.fpga_fft_readout_indexes+1
+            tones = ri.tone_bins[ri.bank,ri.readout_selection]
+            nsamp = ri.tone_nsamp
+            print "saving data"
+            for m in range(len(chids)):
+                print m
+                block = data_block.DataBlock(data = dmod[:,m], tone=tones[m], fftbin = chids[m], 
+                         nsamp = nsamp, nfft = ri.nfft, wavenorm = ri.wavenorm, t0 = time.time(), fs = ri.fs)
+                tsg = df.add_block_to_timestream(block, tsg=tsg)
+        df.sync()
+        time.sleep(30)
 
+    sweep_data = sweeps.do_prepared_sweep(ri, nchan_per_step=atonce, reads_per_step=8)
+    df.add_sweep(sweep_data)
+        
     df.log_hw_state(ri)
         #sc.fetchDict()
         #df.add_cryo_data(sc.data)
