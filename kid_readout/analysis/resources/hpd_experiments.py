faabbaeea735362a39159884e0abdedc3debc700
gitj <glenn.caltech@gmail.com>
2014-08-20 21:36:11 -0400

    Add latest experiments

--- kid_readout/analysis/resources/hpd_experiments.py
+++ kid_readout/analysis/resources/hpd_experiments.py
@@ -1,6 +1,8 @@
 from kid_readout.utils.time_tools import date_to_unix_time
 
-by_unix_time_table = [('2014-04-14', 'STAR Cryo 3x3 0813f5 JPL window package, Al tape cover, encased in copper tamale', 'dark'),
+by_unix_time_table = [('2014-08-11', 'STAR Cryo 3x3 140423 0813f4 JPL window package, Al tape with hole for red LED', 'dark'),
+                      ('2014-07-28', 'STAR Cryo 5x4 0813f12 Aluminum package, taped horns', 'dark'),
+                      ('2014-04-14', 'STAR Cryo 3x3 0813f5 JPL window package, Al tape cover, encased in copper tamale', 'dark'),
                       ('2014-02-21', 'STAR Cryo 3x3 0813f5 JPL window package, Al tape cover part 2', 'dark'),
                       ('2014-02-10', 'STAR Cryo 3x3 0813f5 JPL window package, Al tape cover', 'dark'),
                 ]
