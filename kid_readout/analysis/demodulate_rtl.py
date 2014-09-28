28fce692754035123b788240f9f5fbb73f6b2962
gitj <glenn.caltech@gmail.com>
2014-09-28 07:42:37 -0400

    add folding and rtl time constant processing code

--- kid_readout/analysis/demodulate_rtl.py
+++ kid_readout/analysis/demodulate_rtl.py
@@ -24,7 +24,7 @@ def demodulate(data,poly_degree=7,debug=False):
     if interval == 0:
         interval = 1
     t = np.arange(num_points)
-    poly = np.polyfit(t[::interval],np.unwrap(np.angle(demod[::interval])),poly_degree)
+    poly = np.polyfit(t[4000::interval],np.unwrap(np.angle(demod[4000::interval])),poly_degree)
     demod2 = demod * np.exp(-1j*np.polyval(poly,t))
     if debug:
         fig = plt.figure()
@@ -34,3 +34,25 @@ def demodulate(data,poly_degree=7,debug=False):
         ax.plot(t[::interval],np.unwrap(np.angle(demod2[::interval])))
 
     return demod2
+
+def fold(data,period,debug=False):
+    num_points = data.shape[0]
+    fold1 = data[:period*(num_points//period)].reshape((-1,period))
+    fold1 = fold1[1:,:] #drop first row since it's often got garbage
+    first_rows = np.abs(fold1[:10,:]).mean(0)
+    first_peak = abs(first_rows - first_rows.mean()).argmax()
+    if first_peak < 2000:
+        fold1 = np.roll(fold1,2000,axis=1)
+        first_peak += 2000
+    if first_peak > 8000:
+        fold1 = np.roll(fold1,-8000,axis=1)
+        first_peak = first_peak-8000
+    last_rows = np.abs(fold1[-10:,:]).mean(0)
+    last_peak = abs(last_rows - last_rows.mean()).argmax()
+    slope = (last_peak - first_peak)/float(fold1.shape[0]-10)
+    print last_peak, first_peak, slope
+    fold2 = np.empty_like(fold1)
+    for k in range(fold1.shape[0]):
+        fold2[k,:] = np.roll(fold1[k,:],-int(np.round(slope*k)))
+    fold2 = fold2 * np.exp(-1j*np.angle(fold2[:,:1000]).mean(1)[:,None])
+    return fold2
---
+++ kid_readout/analysis/time_constant_from_rtl.py
@@ -0,0 +1,27 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+import kid_readout.analysis.demodulate_rtl
+reload(kid_readout.analysis.demodulate_rtl)
+import kid_readout.analysis.fit_pulses
+import kid_readout.analysis.fitter
+
+
+def get_time_constant_from_file(filename,pulse_period=10e-3, debug=False):
+    d = np.load(filename)
+    demod = kid_readout.analysis.demodulate_rtl.demodulate(d['data'],debug=False)
+    pulse_period_samples = int(d['sample_rate'][()]*pulse_period)
+    folded = kid_readout.analysis.demodulate_rtl.fold(demod,pulse_period_samples)
+    t = np.arange(pulse_period_samples)/d['sample_rate']
+    y = np.abs(folded).mean(0)
+    fit = kid_readout.analysis.fitter.Fitter(t,y, model=kid_readout.analysis.fit_pulses.fred_model,
+                                             guess = kid_readout.analysis.fit_pulses.fred_guess)
+
+    if debug:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.plot(t,y)
+        ax.plot(t,fit.model(x=t),'r',lw=2)
+        peakt = t[np.abs(y-y.mean()).argmax()]
+        ax.set_xlim(peakt-1e-3,peakt+1e-3)
+    return fit.tau, fit
