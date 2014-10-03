01f6ef694f34ab5cc45cb78d205bd815967eaa37
gitj <glenn.caltech@gmail.com>
2014-10-03 10:58:13 -0400

    delete more old stuff, can always pull it back if needed

--- apps/minicoord_interface.py
+++
@@ -1,8 +0,0 @@
-import Pyro4
-
-ns = Pyro4.naming.locateNS()
-
-uri = ns.lookup("minicoord")
-c = Pyro4.Proxy(uri)
-
-print 'Methods of minicoords are get_data(number_of_packets) and set_channel_ids([list_of_channel_ids])'
