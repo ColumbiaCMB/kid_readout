77eec5d3d0a01dd6d64c98e4993833a696ea08cf
gitj <glenn.caltech@gmail.com>
2014-11-16 17:54:15 -0500

    add a new, hopefully easier option for using MCMC with fitter classes

--- kid_readout/analysis/mcfit.py
+++ kid_readout/analysis/mcfit.py
@@ -27,6 +27,76 @@ def convert_to_lmfit_params(raw_params,base_params,raw_param_list=parameter_list
             lm_params.add(name,value=param.value)
     return lm_params
 
+class GeneralMCMC():
+    def __init__(self, fitter):
+        self.fitter = fitter
+        self.parameter_list = []
+        self.parameter_mins = []
+        self.parameter_maxs = []
+        self.fixed_parameters = []
+        self.update_parameeter_list()
+    def update_parameter_list(self):
+        self.parameter_list = []
+        self.parameter_mins = []
+        self.parameter_maxs = []
+        self.fixed_parameters = []
+        for name,param in self.fitter.result.params.items():
+            if param.vary:
+                self.parameter_list.append(name)
+                self.parameter_mins.append(param.min)
+                self.parameter_maxs.append(param.max)
+            else:
+                self.fixed_parameters.append(name)
+    def set_parameter_values(self,args):
+        if len(args) != len(self.parameter_list):
+            raise ValueError("Got %d values but have %d parameters. values were %s, parameters are %s" %
+                             (len(args), len(self.parameter_list), str(args), str(self.parameter_list)))
+        for name,value in zip(self.parameter_list,args):
+            self.fitter.result.params[name].value = value
+
+    def uniform_logprior(self,args):
+        if len(args) != len(self.parameter_list):
+            raise ValueError("Got %d values but have %d parameters. values were %s, parameters are %s" %
+                             (len(args), len(self.parameter_list), str(args), str(self.parameter_list)))
+
+        for value,min,max in zip(args,self.mins,self.maxs):
+            if value > max or value < min:
+                return -np.inf
+        return 0.
+
+    def basic_loglikelihood(self,params):
+        self.set_parameter_values(params)
+        model = self.fitter.model()
+        return (-np.log(np.abs(self.fitter.errors))
+                - 0.5*abs((self.fitter.y_data-model)/self.fitter.errors)**2)
+    def basic_logprob(self,params):
+        if np.isinf(self.uniform_logprior(params)):
+            return -np.inf
+        return np.sum(self.basic_loglikelihood(params))
+
+    def setup_sampler(self,nwalkers=32,error_factor=None):
+        self.update_parameter_list()
+        ndim = len(self.parameter_list)
+        self.ndim = ndim
+        self.initial = np.zeros((nwalkers,ndim))
+        for dim in range(ndim):
+            min,max = self.parameter_mins[dim], self.parameter_maxs[dim]
+            self.initial[:,dim] = np.random.uniform(min,max,size=nwalkers)
+        self.sampler = emcee.EnsembleSampler(nwalkers,ndim,self.basic_logprob)
+
+    def run(self,length=500,burn_in=100,nwalkers=32):
+        self.setup_sampler(nwalkers=nwalkers)
+        self.sampler.run_mcmc(self.initial,length)
+        self.samples = self.sampler.chain[:,burn_in:,:].reshape((-1,self.ndim))
+        for dim,name in enumerate(self.parameter_list):
+            self.fitter.result.params[name].value = self.samples[:,dim].mean()
+            self.fitter.result.params[name].stderr = self.samples[:,dim].std()
+
+    def triangle(self,*args,**kwargs):
+        import triangle
+        kwargs['labels'] = self.parameter_list
+        triangle.corner(self.samples,*args,**kwargs)
+
 
 class MCMCFitter(Fitter):
     def __init__(self,*args,**kwargs):
