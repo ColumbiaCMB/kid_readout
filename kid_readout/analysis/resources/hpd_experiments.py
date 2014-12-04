bc2f3d2c5604f898c9c9244075b22aa28c8d91c2
gitj <glenn.caltech@gmail.com>
2014-12-04 10:27:35 -0500

    update hpd_experiments to use new experiment info format

--- kid_readout/analysis/resources/hpd_experiments.py
+++ kid_readout/analysis/resources/hpd_experiments.py
@@ -1,12 +1,28 @@
 from kid_readout.utils.time_tools import date_to_unix_time
 
-by_unix_time_table = [('2014-08-11', 'STAR Cryo 3x3 140423 0813f4 JPL window package, Al tape with hole for red LED', 'dark'),
-                      ('2014-07-28', 'STAR Cryo 5x4 0813f12 Aluminum package, taped horns', 'dark'),
-                      ('2014-04-14', 'STAR Cryo 3x3 0813f5 JPL window package, Al tape cover, encased in copper tamale', 'dark'),
-                      ('2014-02-21', 'STAR Cryo 3x3 0813f5 JPL window package, Al tape cover part 2', 'dark'),
-                      ('2014-02-10', 'STAR Cryo 3x3 0813f5 JPL window package, Al tape cover', 'dark'),
-                ]
 
-by_unix_time_table.sort(key = lambda x: date_to_unix_time(x[0]))
-_unix_time_index = [date_to_unix_time(x[0]) for x in by_unix_time_table]
+by_unix_time_table = [
+    dict(date='2014-08-11',
+         description='STAR Cryo 3x3 140423 0813f4 JPL window package, Al tape with hole for red LED',
+         optical_state='dark',
+         chip_id='140423 0813f4'),
+    dict(date='2014-07-28',
+         description='STAR Cryo 5x4 130919 0813f12 Aluminum package, taped horns',
+         optical_state='dark',
+         chip_id='130919 0813f12'),
+    dict(date='2014-04-14',
+         description='STAR Cryo 3x3 0813f5 JPL window package, Al tape cover, encased in copper tamale',
+         optical_state='dark',
+         chip_id='130919 0813f5'),
+    dict(date='2014-02-21',
+         description='STAR Cryo 3x3 0813f5 JPL window package, Al tape cover part 2',
+         optical_state='dark',
+         chip_id='130919 0813f5'),
+    dict(date='2014-02-10',
+         description='STAR Cryo 3x3 0813f5 JPL window package, Al tape cover',
+         optical_state='dark',
+         chip_id='130919 0813f5'),
+    ]
 
+by_unix_time_table.sort(key=lambda x: date_to_unix_time(x['date']))
+_unix_time_index = [date_to_unix_time(x['date']) for x in by_unix_time_table]
