1897f5b67b32cb857526469eddca1217c308b5f3
gitj <glenn.caltech@gmail.com>
2014-12-04 11:05:46 -0500

    add resonator_id to new archive code

--- kid_readout/analysis/archive.py
+++ kid_readout/analysis/archive.py
@@ -10,7 +10,7 @@ from kid_readout.analysis.noise_measurement import load_noise_pkl
 import glob
 import time
 
-def build_simple_archive(pklnames):
+def build_simple_archive(pklnames, index_to_id = None):
     if not type(pklnames) is list:
         pklnames = glob.glob(pklnames)
     data = []
@@ -19,4 +19,12 @@ def build_simple_archive(pklnames):
         for nm in nms:
             data.append(nm.to_dataframe())
     df = pd.concat(data,ignore_index=True)
+    if index_to_id is None:
+        indexes = list(set(df.resonator_index))
+        indexes.sort()
+        index_to_id = indexes
+    def set_resonator_id(x):
+        x['resonator_id'] = index_to_id[x.resonator_index.iloc[0]]
+        return x
+    df = df.groupby(['resonator_index']).apply(set_resonator_id).reset_index(drop=True)
     return df
