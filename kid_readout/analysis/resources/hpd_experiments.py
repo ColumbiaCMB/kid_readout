21a669cf691a105ac4cf948edc8a24720f61ab98
gitj <glenn.caltech@gmail.com>
2014-12-15 11:54:46 -0500

    Update HPD experiments

--- kid_readout/analysis/resources/hpd_experiments.py
+++ kid_readout/analysis/resources/hpd_experiments.py
@@ -2,6 +2,19 @@ from kid_readout.utils.time_tools import date_to_unix_time
 
 
 by_unix_time_table = [
+    dict(date='2014-12-11',
+         description='STAR Cryo 4x5 130919 0813f12 ASU Al horn package, AR chip, aluminum tape over horns, '
+                     'copper shield soldered shut around package, steelcast coax filters, small hole in copper and '
+                     'aluminum shields to let in 1550 nm and red LED light',
+         optical_state='light',
+         chip_id='130919 0813f12',
+    ),
+    dict(date='2014-12-01',
+         description='STAR Cryo 4x5 130919 0813f12 ASU Al horn package, AR chip, aluminum tape over horns, '
+                     'copper shield soldered shut around package, steelcast coax filters',
+         optical_state='dark',
+         chip_id='130919 0813f12',
+    ),
     dict(date='2014-08-11',
          description='STAR Cryo 3x3 140423 0813f4 JPL window package, Al tape with hole for red LED',
          optical_state='dark',
