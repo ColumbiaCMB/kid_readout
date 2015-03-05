90c2780e1317b40f30e19292d3012640f230300d
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2015-03-05 11:04:16 -0500

    responsivity fitting with errors in dataframe

--- kid_readout/analysis/dataframe.py
+++ kid_readout/analysis/dataframe.py
@@ -1,65 +1,217 @@
 """
 This module is for functions that operate on DataFrames.
 
-For consistency, all the functions should return the DataFrame.
+Only the functions that use groupby should return the DataFrame.
 """
 from __future__ import division
+from copy import deepcopy
 import numpy as np
 import pandas as pd
+from kid_readout.analysis.khalil import qi_error
+from kid_readout.analysis import archive
 import lmfit
+
+
+def analyze(df, channel_to_location=None, maximum_fractional_f_r_error=1e-5, maximum_iQi_error=1e-5):
+    rename_f(df)
+    add_Q_i_err(df)
+    add_channel(df)
+    if channel_to_location is not None:
+        add_location(df, channel_to_location)
+    add_zbd_power(df)
+    add_resonator_fit_good(df, maximum_fractional_f_r_error, maximum_iQi_error)
+    df = archive.add_noise_fits(df)
+    df = archive.add_total_mmw_attenuator_turns(df)
+    return df
+
+
+def analyze_mmw_power_steps(df, channel_to_location=None, maximum_X_error=1e-6, maximum_I_error=1e-5,
+                            initial_responsivity=None, masker = lambda g: np.array((g.timestream_modulation_duty_cycle==0) &
+                            g.resonator_fit_good)):
+    pass
+    """
+    if initial_responsivity is None:
+        # implement guess
+        raise NotImplementedError()
+    if masker is None:
+        masker = lambda g: np.array((g.timestream_modulation_duty_cycle==0) &
+                                    g.resonator_fit_good)
+    df = add_responsivities(df, initial_responsivity, masker=masker)
+    df = add_NEP2(df)
+    return df
+    """
+
+def rename_f(df):
+    df.rename(columns={'f_0': 'f_r', 'f_0_err': 'f_r_err'}, inplace=True)
+
+
+# TODO: this should be deprecated when the error on Q_i or Q_i**-1 is computed upstream.
+def add_Q_i_err(df):
+    df['Q_i_err'] = [qi_error(row.Q, row.Q_err, row.Q_e_real, row.Q_e_real_err, row.Q_e_imag, row.Q_e_imag_err)
+                     for index, row in df.iterrows()]
+
 
 # This should work except in pathological cases; groupby sorts by key automatically.
 def add_channel(df):
-    resonator_index_to_channel = np.array([group.f_0.mean() for index, group in df.groupby('resonator_index')]).argsort()
+    resonator_index_to_channel = np.array(
+        [group.f_r.mean() for index, group in df.groupby('resonator_index')]).argsort()
     df['channel'] = resonator_index_to_channel[np.array(df.resonator_index)]
-    return df
 
 
 def add_location(df, channel_to_location):
     df['location'] = channel_to_location[np.array(df.channel)]
-    return df
 
 
+# TODO: upgrade to use ZBD responsivity and account for lock-in
 def add_zbd_power(df, zbd_volts_per_watt=2200):
     df['zbd_power'] = df.zbd_voltage / zbd_volts_per_watt
+
+
+def add_resonator_fit_good(df, maximum_fractional_f_r_error, maximum_iQi_error):
+    df['resonator_fit_good'] = ((df.f_r_err / df.f_r < maximum_fractional_f_r_error) &
+                                (df.Q_i_err / df.Q_i**2 < maximum_iQi_error) &
+                                (df.Q_i > 0))  # This shouldn't be necessary, but it currently is.
+
+
+# Response fitting
+# TODO: move the fitting functions to another module and rename them.
+def n_qp_over_n_star(P, P_0, P_star):
+    return (1 + (P + P_0) / P_star) ** (1 / 2) - 1
+
+
+def X(P, P_0, P_star, X_0):
+    return X_0 * n_qp_over_n_star(P, P_0, P_star)
+
+
+def X_model(params, P):
+    return X(P, params['P_0'].value, params['P_star'].value, params['X_0'].value)
+
+
+def X_inverse(X, P_0, P_star, X_0):
+    return P_star * ((1 + X / X_0) ** 2 - 1) - P_0
+
+
+def X_residual(params, P, X_data, X_errors):
+    return (X_data - X_model(params, P)) / X_errors
+
+
+def I(P, P_0, P_star, I_0, I_C):
+    return I_0 * n_qp_over_n_star(P, P_0, P_star) + I_C
+
+
+def I_model(params, P):
+    return I(P, params['P_0'].value, params['P_star'].value, params['I_0'].value, params['I_C'].value)
+
+
+def I_inverse(I, P_0, P_star, I_0, I_C):
+    return P_star * ((1 + (I - I_C) / I_0) ** 2 - 1) - P_0
+
+
+def I_residual(params, P, I_data, I_errors):
+    return (I_data - I_model(params, P)) / I_errors
+
+
+def residual(params, P, X_data, I_data, X_errors, I_errors):
+    return np.concatenate((X_residual(params, P, X_data, X_errors),
+                           I_residual(params, P, I_data, I_errors)))
+
+
+def fit(P, X_data, I_data, initial, X_errors=1, I_errors=1, **kwargs):
+    return lmfit.minimize(residual, initial, args=(P, X_data, I_data, X_errors, I_errors), **kwargs)
+
+
+def guess(P, X_data, I_data):
+    return parameters(4e-8, 1e-6, 20e-6, 5e-6, 5e-6)
+
+
+def parameters(P_0, P_star, X_0, I_0, I_C):
+    params = lmfit.Parameters()
+    params.add('P_0', value=P_0, min=0)
+    params.add('P_star', value=P_star, min=0)
+    params.add('X_0', value=X_0, min=0)
+    params.add('I_0', value=I_0, min=0)
+    params.add('I_C', value=I_C, min=0)
+    return params
+
+
+def dX_dP(P, P_0, P_star, X_0):
+    return X_0 / (2 * n_qp_over_n_star(P, P_0, P_star))
+
+
+def dI_dP(P, P_0, P_star, I_0):
+    return I_0 / (2 * n_qp_over_n_star(P, P_0, P_star))
+
+
+def add_XI_responsivity(df, power='zbd_power', masker=None):
+    def XI_responsivity(group):
+        if masker is None:
+            mask = np.ones(group.shape[0], dtype=np.bool)
+        else:
+            mask = masker(group)
+        #group.sort(power, inplace=True)
+        f_r_max_row = group[mask][group[mask].f_r == group[mask].f_r.max()]
+        f_r_max = float(f_r_max_row.f_r)
+        f_r_max_err = float(f_r_max_row.f_r_err)
+        group.loc[mask, '{}_f_r_max'.format(power)] = f_r_max
+        group.loc[mask, '{}_f_r_max_err'.format(power)] = f_r_max_err
+        group.loc[mask, '{}_X'.format(power)] = f_r_max / group.f_r - 1
+        group.loc[mask, '{}_X_err'.format(power)] = ((f_r_max_err / f_r_max)**2 + (group.f_r_err / group.f_r)**2)**(1/2)
+        group.loc[mask, '{}_I'.format(power)] = group.Q_i**-1
+        group.loc[mask, '{}_I_err'.format(power)] = group.Q_i**-2 * group.Q_i_err
+        return group
+    return df.groupby(('channel', 'atten')).apply(XI_responsivity).reset_index(drop=True)
+
+
+# Remove bad data and power-off points before fitting.
+def fit_XI_responsivity(df, initial, power='zbd_power', masker=lambda group: np.ones(group.shape[0], dtype=np.bool)):
+    def XI_responsivity(group):
+        mask = masker(group)
+        # Add try...except for cases where the fit fails because too many points are masked.
+        try:
+            result = fit(group[mask][power], group[mask]['{}_X'.format(power)], group[mask]['{}_I'.format(power)],
+                         deepcopy(initial),  # This is crucial because minimize modifies the input Parameters.
+                         X_errors=group[mask]['{}_X_err'.format(power)], I_errors=group[mask]['{}_I_err'.format(power)])
+            for p in result.params.values():
+                group.loc[mask, '{}_XI_fit_{}'.format(power, p.name)] = p.value
+                group.loc[mask, '{}_XI_fit_{}_err'.format(power, p.name)] = p.stderr
+            group.loc[mask, '{}_XI_fit_redchi'.format(power)] = result.redchi
+            group.loc[mask, '{}_dX_dP'.format(power)] = dX_dP(group[power], result.params['P_0'].value,
+                                                              result.params['P_star'].value, result.params['X_0'].value)
+            group.loc[mask, '{}_dI_dP'.format(power)] = dI_dP(group[power], result.params['P_0'].value,
+                                                              result.params['P_star'].value, result.params['I_0'].value)
+        except TypeError:
+            pass
+        return group
+    return df.groupby(('channel', 'atten')).apply(XI_responsivity).reset_index(drop=True)
+
+
+def add_NEP2(df, power='zbd_power'):
+    df['{}_NEP2_device'.format(power)] = df.noise_fit_device_noise / df['{}_dX_dP'.format(power)] ** 2
+    df['{}_NEP2_amplifier'.format(power)] = df.noise_fit_amplifier_noise / df['{}_dX_dP'.format(power)] ** 2
     return df
 
-
-# Mask bad data before calling.
-def add_load_temperature_responsivities(df):
-    def calculate_load_temperature_responsivities(group):
-        group.sort('sweep_primary_load_temperature', inplace=True)
-        mask = group.timestream_modulation_duty_cycle == 1  # 1 is source off and 0 is source on
-        group.loc[mask, 'f_0_max'] = group[mask].f_0.max()
-        group.loc[mask, 'x'] = group[mask].f_0 / group.f_0_max - 1
-        T_load_x_fit = np.polyfit(group[mask].sweep_primary_load_temperature, group[mask].x, 1)
-        group.loc[mask, 'dx_dT'] = T_load_x_fit[0]
-        group.loc[mask, 'T_load_x_fit_1'] = T_load_x_fit[1]
-        T_load_iQi_fit = np.polyfit(group[mask].sweep_primary_load_temperature, group[mask].Q_i**-1, 1)
-        group.loc[mask, 'diQi_dT'] = T_load_iQi_fit[0]
-        group.loc[mask, 'T_load_iQi_fit_1'] = T_load_iQi_fit[1]
-        return group
-    return df.groupby(('channel', 'atten')).apply(calculate_load_temperature_responsivities).reset_index(drop=True)
-
-
-# TODO: move the fitting functions to another module and rename them.
+# TODO: these are deprecated; remove
 def z(params, P):
     z_0 = params['z_0'].value
     P_0 = params['P_0'].value
     P_star = params['P_star'].value
-    return z_0 * ((1 + (P + P_0) / P_star)**(1/2) - 1)
+    return z_0 * ((1 + (P + P_0) / P_star) ** (1 / 2) - 1)
+
 
 def dz_dP(params, P):
     z_0 = params['z_0'].value
     P_0 = params['P_0'].value
     P_star = params['P_star'].value
-    return z_0 / (2 * P_star) * (1 + (P + P_0) / P_star)**(-1/2)
+    return z_0 / (2 * P_star) * (1 + (P + P_0) / P_star) ** (-1 / 2)
+
 
 def z_residual(params, P, data, errors=1):
     return (data - z(params, P)) / errors
 
-def fit_z(P, data, initial, errors=1):
-    return lmfit.minimize(z_residual, initial, args=(P, data, errors))
+
+def fit_z(P, data, initial, errors=1, **kwargs):
+    return lmfit.minimize(z_residual, initial, args=(P, data, errors), **kwargs)
+
 
 def z_initial(z_0=1, P_0=1, P_star=1):
     params = lmfit.Parameters()
@@ -68,11 +220,12 @@ def z_initial(z_0=1, P_0=1, P_star=1):
     params.add('P_star', value=P_star, min=0)
     return params
 
+
 def z_inverse(params, z):
     z_0 = params['z_0'].value
     P_0 = params['P_0'].value
     P_star = params['P_star'].value
-    return P_star * ((1 + z / z_0)**2 - 1) - P_0
+    return P_star * ((1 + z / z_0) ** 2 - 1) - P_0
 
 
 # Mask bad data before fitting.
@@ -80,22 +233,25 @@ def add_zbd_power_responsivities(df):
     def calculate_zbd_power_responsivities(group):
         group.sort('zbd_power', inplace=True)
         mask = group.timestream_modulation_duty_cycle == 0  # 1 is source off and 0 is source on
-        group.loc[mask, 'f_0_max'] = group[mask].f_0.max()
-        group.loc[mask, 'x'] = group.f_0 / group.f_0_max - 1
+        group.loc[mask, 'f_r_max'] = group[mask].f_r.max()
+        group.loc[mask, 'x'] = group.f_r / group.f_r_max - 1
 
-        P_zbd_x_fit = fit_z(group[mask].zbd_power, group[mask].x, z_initial(-10e-6, 10e-6, 10e-6))
+        P_zbd_x_fit = fit_z(group[mask].zbd_power, group[mask].x, z_initial(-10e-6, 10e-6, 10e-6),
+                            xtol=1e-10, ftol=1e-10)  # Experimental.
         group.loc[mask, 'P_zbd_x_fit_z_0'] = P_zbd_x_fit.params['z_0'].value
         group.loc[mask, 'P_zbd_x_fit_P_0'] = P_zbd_x_fit.params['P_0'].value
         group.loc[mask, 'P_zbd_x_fit_P_star'] = P_zbd_x_fit.params['P_star'].value
         group.loc[mask, 'dx_dP'] = dz_dP(P_zbd_x_fit.params, group[mask].zbd_power)
 
-        P_zbd_iQi_fit = fit_z(group[mask].zbd_power, 1 / group[mask].Q_i, z_initial(10e-6, 1e-6, 10e-6))
+        P_zbd_iQi_fit = fit_z(group[mask].zbd_power, 1 / group[mask].Q_i, z_initial(10e-6, 1e-6, 10e-6),
+                              xtol=1e-10, ftol=1e-10)  # Experimental.
         group.loc[mask, 'P_zbd_iQi_fit_z_0'] = P_zbd_iQi_fit.params['z_0'].value
         group.loc[mask, 'P_zbd_iQi_fit_P_0'] = P_zbd_iQi_fit.params['P_0'].value
         group.loc[mask, 'P_zbd_iQi_fit_P_star'] = P_zbd_iQi_fit.params['P_star'].value
         group.loc[mask, 'diQi_dP'] = dz_dP(P_zbd_iQi_fit.params, group[mask].zbd_power)
 
         return group
+
     return df.groupby(('channel', 'atten')).apply(calculate_zbd_power_responsivities).reset_index(drop=True)
 
 
@@ -103,24 +259,24 @@ def add_zbd_power_responsivities(df):
 # noise, while noise_fit_device_noise does not.
 
 def add_NET2(df):
-    df['NET2_device'] = df.device_noise / (2 * df.dx_dT**2)
-    df['NET2_amplifier'] = df.amplifier_noise / (2 * df.dx_dT**2)
+    df['NET2_device'] = df.device_noise / (2 * df.dx_dT ** 2)
+    df['NET2_amplifier'] = df.amplifier_noise / (2 * df.dx_dT ** 2)
     return df
 
 
 def add_NET2_fit(df):
-    df['NET2_fit_device'] = df.noise_fit_device_noise / (2 * df.dx_dT**2)
-    df['NET2_fit_amplifier'] = df.noise_fit_amplifier_noise / (2 * df.dx_dT**2)
+    df['NET2_fit_device'] = df.noise_fit_device_noise / (2 * df.dx_dT ** 2)
+    df['NET2_fit_amplifier'] = df.noise_fit_amplifier_noise / (2 * df.dx_dT ** 2)
     return df
 
 
 def add_NEP2_zbd(df):
-    df['NEP2_zbd_device'] = df.device_noise / df.dx_dP**2
-    df['NEP2_zbd_amplifier'] = df.amplifier_noise / df.dx_dP**2
+    df['NEP2_zbd_device'] = df.device_noise / df.dx_dP ** 2
+    df['NEP2_zbd_amplifier'] = df.amplifier_noise / df.dx_dP ** 2
     return df
 
 
 def add_NEP2_zbd_fit(df):
-    df['NEP2_zbd_fit_device'] = df.noise_fit_device_noise / df.dx_dP**2
-    df['NEP2_zbd_fit_amplifier'] = df.noise_fit_amplifier_noise / df.dx_dP**2
+    df['NEP2_zbd_fit_device'] = df.noise_fit_device_noise / df.dx_dP ** 2
+    df['NEP2_zbd_fit_amplifier'] = df.noise_fit_amplifier_noise / df.dx_dP ** 2
     return df
--- kid_readout/analysis/plot_dataframe.py
+++ kid_readout/analysis/plot_dataframe.py
@@ -1,5 +1,7 @@
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
+from kid_readout.analysis import dataframe
 
 def plot_sweeps(df, n, key='channel', legend=False):
     fig, ax = plt.subplots()
@@ -13,6 +15,7 @@ def plot_sweeps(df, n, key='channel', legend=False):
         ax.legend(loc='best')
     ax.set_title('{}: {}'.format(key, n))
     return fig, ax
+
 
 def plot_sweeps_IQ(df, n, key='channel', legend=False):
     fig, ax = plt.subplots()
@@ -29,6 +32,7 @@ def plot_sweeps_IQ(df, n, key='channel', legend=False):
     ax.set_title('{}: {}'.format(key, n))
     return fig, ax
 
+
 def plot_pca_noise(df, n, key='channel', legend=False):
     fig, ax = plt.subplots()
     r = df[(df[key]==n)]
@@ -36,8 +40,95 @@ def plot_pca_noise(df, n, key='channel', legend=False):
         for index, row in atten_group.iterrows():
             ax.loglog(row.pca_freq, row.pca_eigvals[1], label=str(atten))
             ax.loglog(row.pca_freq, row.pca_eigvals[0])
-    ax.set_xlim(1e-3, 1e4)
+    #ax.set_xlim(1e-3, 1e4)
     if legend:
         ax.legend(loc='best')
     ax.set_title('{}: {}'.format(key, n))
     return fig, ax
+
+
+def plot_responsivity(df, nrows=4, ncols=4, figsize=(6, 6), loglog=False, power='zbd_power', fits=True, P_scale=1e6,
+                      P_limits=None, X_scale=1e6, X_limits=None, I_scale=1e6, I_limits=None, X_color='blue',
+                      I_color='green', P_label='power [$\mu$W]', X_label='$10^6 X$', I_label='$10^6 Q_i^{-1}$'):
+    masked = df[~df['{}_XI_fit_redchi'.format(power)].isnull()]
+    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+
+    if loglog:
+        P_minimum = 10**(np.floor(np.log10(masked[power].min())) - 1)
+        P_maximum = 10**(np.ceil(np.log10(masked[power].max())) + 1)
+        # The minimum value of X is always 0 so we can't display it on the plot, but we can show the top of the errorbar.
+        X_minimum = 10**np.floor(np.log10(masked['{}_X_err'.format(power)].min()))
+        X_maximum = 10**np.ceil(np.log10(masked['{}_X'.format(power)].max()))
+        I_minimum = 10**np.floor(np.log10(masked['{}_I'.format(power)].min()))
+        I_maximum = 10**np.ceil(np.log10(masked['{}_I'.format(power)].max()))
+        if P_limits is None:
+            P_limits = (P_scale * P_minimum, P_scale * P_maximum)
+        if X_limits is None:
+            X_limits = (X_scale * X_minimum, X_scale * X_maximum)
+        if I_limits is None:
+            I_limits = (I_scale * I_minimum, I_scale * I_maximum)
+        P_fit = np.logspace(np.log10(P_minimum), np.log10(P_maximum), 1e3)
+    else:
+        P_minimum = - 1 / P_scale
+        P_maximum = np.ceil(P_scale * 1.1 * masked[power].max()) / P_scale
+        if P_limits is None:
+            P_limits = (P_scale * P_minimum, P_scale * P_maximum)
+        if X_limits is None:
+            X_limits = (-10, np.ceil(X_scale * masked['{}_X'.format(power)].max() / 100) * 100)
+        if I_limits is None:
+            I_limits = (-1, np.ceil(I_scale * masked['{}_I'.format(power)].max() / 10) * 10)
+        P_fit = np.linspace(P_minimum, P_maximum, 1e3)
+
+    for plot_index, (X_ax, (channel, group)) in enumerate(zip(axes.flatten(), masked.groupby('channel'))):
+        X_ax.errorbar(P_scale * group[power], X_scale * group['{}_X'.format(power)],
+                      yerr=X_scale * group['{}_X_err'.format(power)], marker='.', color=X_color, linestyle='None')
+        I_ax = X_ax.twinx()
+        I_ax.errorbar(P_scale * group[power], I_scale * group['{}_I'.format(power)],
+                      yerr=I_scale * group['{}_I_err'.format(power)], marker='.', color=I_color, linestyle='None')
+
+        if fits:
+            P_0 = group['{}_XI_fit_P_0'.format(power)].iloc[0]
+            P_star = group['{}_XI_fit_P_star'.format(power)].iloc[0]
+            X_0 = group['{}_XI_fit_X_0'.format(power)].iloc[0]
+            I_0 = group['{}_XI_fit_I_0'.format(power)].iloc[0]
+            I_C = group['{}_XI_fit_I_C'.format(power)].iloc[0]
+            X_ax.plot(P_scale * P_fit, X_scale * dataframe.X(P_fit, P_0, P_star, X_0), color=X_color)
+            I_ax.plot(P_scale * P_fit, I_scale * dataframe.I(P_fit, P_0, P_star, I_0, I_C), color=I_color)
+
+        if loglog:
+            X_ax.set_xscale('log')
+            X_ax.set_yscale('log', nonposy='clip')
+            I_ax.set_xscale('log')
+            I_ax.set_yscale('log', nonposy='clip')
+
+        X_ax.set_xlim(*P_limits)
+        X_ax.set_ylim(*X_limits)
+        I_ax.set_ylim(*I_limits)
+        #if P_ticks is not None:
+        #    X_ax.set_xticks(P_ticks)
+
+        X_ax.tick_params(axis='y', labelcolor=X_color)
+        # Put tick labels only on the left edge.
+        if plot_index % ncols != 0:
+            X_ax.set_yticklabels([])
+        I_ax.tick_params(axis='y', labelcolor=I_color)
+        # Put ticks only on the right edge.
+        if plot_index % ncols != ncols - 1:
+            I_ax.set_yticklabels([])
+
+        # Put ticks only on the bottom edge.
+        if plot_index < (nrows - 1) * ncols:
+            X_ax.set_xticklabels([])
+        # Add axis labels.
+        if plot_index == (nrows - 1) * ncols:
+            X_ax.set_xlabel(P_label)
+            X_ax.set_ylabel(X_label)
+        if plot_index == (nrows * ncols - 1):
+            I_ax.set_ylabel(I_label)
+
+        try:
+            X_ax.set_title('{}: {}'.format(group.channel.iloc[0], group.location.iloc[0]))
+        except AttributeError:
+            X_ax.set_title('{}'.format(group.channel.iloc[0]))
+
+    return fig, axes
