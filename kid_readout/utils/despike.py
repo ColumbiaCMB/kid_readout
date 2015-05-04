73f3fb7607e896c2bd5861f29c3f70b8d6ee74e8
gitj <glenn.caltech@gmail.com>
2015-05-04 11:26:47 -0400

    fix minor bug in deglitching

--- kid_readout/utils/despike.py
+++ kid_readout/utils/despike.py
@@ -15,7 +15,7 @@ def medmadmask(ts,thresh=8,axis=0):
     return mask
 
 def deglitch_block(ts,thresh=5):
-    tsl = np.roll(np.abs(fftfilt(scipy.signal.firwin(16,1/16.),ts)),-16)
+    tsl = np.roll(np.abs(fftfilt(scipy.signal.firwin(16,1/16.),ts)),-8)
     mask = medmadmask(tsl,thresh=thresh)
     mask[:-50] = mask[:-50] | mask[50:]
     mask[50:] = mask[50:] | mask[:-50]
