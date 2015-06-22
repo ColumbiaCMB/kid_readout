ac628c6b6399e1a97ed520c5911cfa36d56d171f
gitj <glenn.caltech@gmail.com>
2015-06-22 02:06:17 -0400

    add autoranging measurement to lockin

--- kid_readout/equipment/lockin_controller.py
+++ kid_readout/equipment/lockin_controller.py
@@ -7,21 +7,19 @@ time_constants = ((np.array([1,3])[None,:])*((10.**np.arange(-5,5))[:,None])).fl
 
 
 class lockinController():
-### Initialization ###
     def __init__(self, serial_port='/dev/ttyUSB2', terminator='\n'):
-        # For linux, theself.serial port will be something like the default address.
+        # For linux, the serial port will be something like the default address.
         # However, for windows it will be something like 'COM6'.
         self.address=serial_port
         self.baudrate=19200
         self.terminator=terminator
         self.ser=serial.Serial(self.address, baudrate=self.baudrate,timeout=1,rtscts=True)
-        #self.ser.close()
+        self.time_constant_value = -1
+        self.sensitivity_value = -1
         self.setup_rs232_output()
-        
-    
+
+
     def setup_rs232_output(self):
-        #ser=serial.Serial(self.address, baudrate=self.baudrate)
-        #self.ser.open()
         try:
            self.ser.write('OUTX 0'+self.terminator)
         except Exception as e:
@@ -29,13 +27,8 @@ class lockinController():
             raise e
         finally:
             pass
-#            self.ser.close()
-
-### Basic send and receive methods ###
 
     def send(self,msg):
-       #self.ser=serial.Serial(self.address, baudrate=self.baudrate)
-#        self.ser.open()
         time.sleep(.1)
         try:
             self.ser.write(msg+self.terminator)
@@ -44,11 +37,8 @@ class lockinController():
             raise e
         finally:
             pass
-#            self.ser.close()
 
     def send_and_receive(self,msg):
-        #self.ser=serial.Serial(self.address, baudrate=self.baudrate, timeout=2)
-#        self.ser.open()
         time.sleep(.1)
         # This delay is necessary... for some reason. The system behaves very poorly otherwise.
         # Perhaps theself.serial port takes some time to initialize?
@@ -60,7 +50,49 @@ class lockinController():
             raise e
         finally:
             pass
-#            self.ser.close()
+
+    def get_sensitivity(self):
+        response = int(self.send_and_receive('SENS?'))
+        self.sensitivity_value = response
+        return response, sensitivities[response]
+
+    def set_sensitivity(self,sensitivity_volts):
+        idx = sensitivities.searchsorted(sensitivity_volts)
+        if idx >= len(sensitivities):
+            idx = len(sensitivities)-1
+        self.send("SENS %d" % idx)
+        self.sensitivity_value = idx
+
+    def get_time_constant(self):
+        response = int(self.send_and_receive('OFLT?'))
+        self.time_constant_value = response
+        return response, time_constants[response]
+
+    def auto_range_measure(self,debug=False):
+        if self.sensitivity_value < 0:
+            idx,sens = self.get_sensitivity()
+            if debug:
+                print "initialized sensitivity:", idx,sens
+        x,y,r,theta = self.get_data()
+        sens = sensitivities[self.sensitivity_value]
+        if debug:
+            print "initial sensitivity: %g, initial measurement %g" % (sens,r)
+        while r > sens/2.0:
+            self.set_sensitivity(r*10)
+            sens = sensitivities[self.sensitivity_value]
+            if debug:
+                print "setting sensitivity to %g. new value %g" % (r*10,sens)
+            time.sleep(0.1)
+            x,y,r,theta = self.get_data()
+        if sens > 100e-9:
+            while r < sens/1000.0:
+                self.set_sensitivity(r/100.0)
+                sens = sensitivities[self.sensitivity_value]
+                if debug:
+                    print "setting sensitivity to %g. new value %g" % (r/1000,sens)
+                time.sleep(0.1)
+                x,y,r,theta = self.get_data()
+        return r, sens
 
     def read_until_terminator(self):
         message=''
@@ -74,7 +106,7 @@ class lockinController():
             message+=new_char
         return message
 
-### Stub methods ###
+    ### Stub methods ###
 
     def get_data(self):
         data_string = self.get_snap()
