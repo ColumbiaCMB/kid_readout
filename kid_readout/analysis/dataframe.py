Correct ZBD power calculation in dataframe

kid_readout/analysis/dataframe.py
@@ -7,8 +7,10 @@ from __future__ import division
 from copy import deepcopy
 import numpy as np
 import lmfit
+from equipment.VDI import zbd
 from kid_readout.analysis.khalil import qi_error
 from kid_readout.analysis import archive
+
 
 
 def analyze(df, channel_to_location=None, maximum_fractional_f_r_error=1e-5, maximum_iQi_error=1e-5):
@@ -19,32 +21,20 @@ def analyze(df, channel_to_location=None, maximum_fractional_f_r_error=1e-5, max
         add_location(df, channel_to_location)
     add_resonator_fit_good(df, maximum_fractional_f_r_error, maximum_iQi_error)
     df = archive.add_noise_fits(df)
-    try:
-        add_zbd_power(df)
-    except AttributeError:
-        pass
-    try:
-        df = archive.add_total_mmw_attenuator_turns(df)
-    except AttributeError:
-        pass
     return df
 
 
-def analyze_mmw_power_steps(df, channel_to_location=None, maximum_X_error=1e-6, maximum_I_error=1e-5,
-                            initial_responsivity=None, masker = lambda g: np.array((g.timestream_modulation_duty_cycle==0) &
-                            g.resonator_fit_good)):
-    pass
-    """
-    if initial_responsivity is None:
-        # implement guess
-        raise NotImplementedError()
-    if masker is None:
-        masker = lambda g: np.array((g.timestream_modulation_duty_cycle==0) &
-                                    g.resonator_fit_good)
-    df = add_responsivities(df, initial_responsivity, masker=masker)
-    df = add_NEP2(df)
+def analyze_mmw_source_data(df, zbd_fraction, optical_frequency=None):
+    try:
+        df.rename(columns={'zbd_voltage': 'lockin_rms_voltage'}, inplace=True)
+    except AttributeError:
+        pass
+    add_zbd_voltage(df)
+    add_zbd_power(df, optical_frequency)
+    add_source_power(df, zbd_fraction)
+    df = archive.add_total_mmw_attenuator_turns(df)
     return df
-    """
+
 
 def rename_f(df):
     df.rename(columns={'f_0': 'f_r', 'f_0_err': 'f_r_err'}, inplace=True)
@@ -67,9 +57,34 @@ def add_location(df, channel_to_location):
     df['location'] = channel_to_location[np.array(df.channel)]
 
 
-# TODO: upgrade to use ZBD responsivity and account for lock-in
-def add_zbd_power(df, zbd_volts_per_watt=2200):
-    df['zbd_power'] = df.zbd_voltage / zbd_volts_per_watt
+def add_zbd_voltage(df):
+    """
+    The modulated square wave has minimum 0 and maximum V_z, equivalent to an offset square wave with peak V_z / 2.
+    The lock-in measures the Fourier component at the reference frequency and reports RMS voltage, so in this case it
+    will report
+    V_l = (V_z / 2) (4 / \pi) 2^{-1/2} = (2^{1/2} / \pi) V_z.
+    The peak ZBD voltage is thus
+    V_z = 2^{-1/2} \pi V_l
+    """
+    df['zbd_voltage'] = 2**(-1/2) * np.pi * df.lockin_rms_voltage
+
+
+def add_zbd_power(df, optical_frequency=None):
+    """
+    :param df: The dataframe.
+    :param optical_frequency: The frequency in hertz detected by the ZBD.
+    :return: None; the dataframe is modified.
+    """
+    if optical_frequency is None:
+        zbd_volts_per_watt = 2200
+    else:
+        z = zbd.ZBD()
+        zbd_volts_per_watt = z.responsivity(optical_frequency)
+    df['zbd_power'] = df['zbd_voltage'] / zbd_volts_per_watt
+
+
+def add_source_power(df, zbd_fraction):
+    df['source_power'] = df.zbd_power / zbd_fraction
 
 
 def add_resonator_fit_good(df, maximum_fractional_f_r_error, maximum_iQi_error):
@@ -148,8 +163,8 @@ def parameters(P_0, P_star, X_0, I_0, I_C):
     return params
 
 
-def add_XI_responsivity(df, power='zbd_power', masker=None):
-    def XI_responsivity(group):
+def add_XI_response(df, power, masker=None):
+    def XI_response(group):
         if masker is None:
             mask = np.ones(group.shape[0], dtype=np.bool)
         else:
@@ -166,12 +181,12 @@ def add_XI_responsivity(df, power='zbd_power', masker=None):
             group.loc[mask, '{}_I'.format(power)] = group.Q_i**-1
             group.loc[mask, '{}_I_err'.format(power)] = group.Q_i**-2 * group.Q_i_err
         return group
-    return df.groupby(('channel', 'atten')).apply(XI_responsivity).reset_index(drop=True)
+    return df.groupby(('channel', 'atten')).apply(XI_response).reset_index(drop=True)
 
 
 # Remove bad data and power-off points before fitting.
-def fit_XI_responsivity(df, initial, power='zbd_power', masker=lambda group: np.ones(group.shape[0], dtype=np.bool)):
-    def XI_responsivity(group):
+def fit_XI_response(df, initial, power, masker=lambda group: np.ones(group.shape[0], dtype=np.bool)):
+    def XI_response(group):
         mask = masker(group)
         try:
             result = fit(group[mask][power], group[mask]['{}_X'.format(power)], group[mask]['{}_I'.format(power)],
@@ -188,10 +203,10 @@ def fit_XI_responsivity(df, initial, power='zbd_power', masker=lambda group: np.
         except TypeError:
             pass
         return group
-    return df.groupby(('channel', 'atten')).apply(XI_responsivity).reset_index(drop=True)
+    return df.groupby(('channel', 'atten')).apply(XI_response).reset_index(drop=True)
 
 
-def add_NEP2(df, power='zbd_power'):
+def add_NEP2(df, power):
     df['{}_NEP2_device'.format(power)] = df.noise_fit_device_noise / df['{}_dX_dP'.format(power)] ** 2
     df['{}_NEP2_amplifier'.format(power)] = df.noise_fit_amplifier_noise / df['{}_dX_dP'.format(power)] ** 2
 

kid_readout/analysis/plot_dataframe.py
@@ -4,16 +4,21 @@ import matplotlib
 import matplotlib.pyplot as plt
 from kid_readout.analysis import dataframe
 
-def plot_sweeps(df, k, select_key='channel', color_key='zbd_power', norm=matplotlib.colors.LogNorm,
+def plot_sweeps(df, k, select_key='channel', color_key=None, norm=matplotlib.colors.LogNorm,
                 colormap=plt.cm.coolwarm, legend=False):
     fig, ax = plt.subplots()
     r = df[df[select_key]==k]
-    mappable = plt.cm.ScalarMappable(norm(min(df[color_key]), max(df[color_key])), cmap=colormap)
-    mappable.set_array(df[color_key])
+    if color_key is not None:
+        mappable = plt.cm.ScalarMappable(norm(min(df[color_key]), max(df[color_key])), cmap=colormap)
+        mappable.set_array(df[color_key])
     for atten, atten_group in r.groupby('atten'):
         for index, row in atten_group.iterrows():
+            if color_key is None:
+                color = 'black'
+            else:
+                color = mappable.to_rgba(row[color_key])
             ax.plot(row.sweep_freqs_MHz, 20*np.log10(abs(row.sweep_normalized_s21)), label=str(atten),
-                    color=mappable.to_rgba(row[color_key]))
+                    color=color)
     ax.set_xlabel('frequency [MHz]')
     ax.set_ylabel('$S_{21}$ [dB]')
     if legend:
@@ -22,16 +27,21 @@ def plot_sweeps(df, k, select_key='channel', color_key='zbd_power', norm=matplot
     return fig, ax
 
 
-def plot_sweeps_IQ(df, k, select_key='channel', color_key='zbd_power', norm=matplotlib.colors.LogNorm,
+def plot_sweeps_IQ(df, k, select_key='channel', color_key=None, norm=matplotlib.colors.LogNorm,
                    colormap=plt.cm.coolwarm, legend=False):
     fig, ax = plt.subplots()
     r = df[df[select_key]==k]
-    mappable = plt.cm.ScalarMappable(norm(min(df[color_key]), max(df[color_key])), cmap=colormap)
-    mappable.set_array(df[color_key])
+    if color_key is not None:
+        mappable = plt.cm.ScalarMappable(norm(min(df[color_key]), max(df[color_key])), cmap=colormap)
+        mappable.set_array(df[color_key])
     for atten, atten_group in r.groupby('atten'):
         for index, row in atten_group.iterrows():
+            if color_key is None:
+                color = 'black'
+            else:
+                color = mappable.to_rgba(row[color_key])
             ax.plot(row.sweep_normalized_s21.real, row.sweep_normalized_s21.imag, label=str(atten),
-                    color=mappable.to_rgba(row[color_key]))
+                    color=color)
     ax.set_xlabel('Re $S_{21}$')
     ax.set_ylabel('Im $S_{21}$')
     ax.set_xlim(0, 1.8)
@@ -42,25 +52,30 @@ def plot_sweeps_IQ(df, k, select_key='channel', color_key='zbd_power', norm=matp
     return fig, ax
 
 
-def plot_pca_noise(df, k, select_key='channel', color_key='zbd_power', norm=matplotlib.colors.LogNorm,
+def plot_pca_noise(df, k, select_key='channel', color_key=None, norm=matplotlib.colors.LogNorm,
                    colormap=plt.cm.coolwarm, legend=False, plot_row_1=True, plot_row_0=True):
     fig, ax = plt.subplots()
     r = df[(df[select_key]==k)]
-    mappable = plt.cm.ScalarMappable(norm(min(df[color_key]), max(df[color_key])), cmap=colormap)
-    mappable.set_array(df[color_key])
+    if color_key is not None:
+        mappable = plt.cm.ScalarMappable(norm(min(df[color_key]), max(df[color_key])), cmap=colormap)
+        mappable.set_array(df[color_key])
     for atten, atten_group in r.groupby('atten'):
         for index, row in atten_group.iterrows():
+            if color_key is None:
+                color = 'black'
+            else:
+                color = mappable.to_rgba(row[color_key])
             if plot_row_1:
-                ax.loglog(row.pca_freq, row.pca_eigvals[1], label=str(atten), color=mappable.to_rgba(row[color_key]))
+                ax.loglog(row.pca_freq, row.pca_eigvals[1], label=str(atten), color=color)
             if plot_row_0:
-                ax.loglog(row.pca_freq, row.pca_eigvals[0], color=mappable.to_rgba(row[color_key]))
+                ax.loglog(row.pca_freq, row.pca_eigvals[0], color=color)
     if legend:
         ax.legend(loc='best')
     ax.set_title('{}: {}'.format(select_key, k))
     return fig, ax
 
 
-def plot_responsivity(df, nrows=4, ncols=4, figsize=(6, 6), loglog=False, power='zbd_power', fits=True,
+def plot_response(df, power, nrows=4, ncols=4, figsize=(6, 6), loglog=False, fits=True,
                       P_scale=1e6, P_limits=None, P_label='power [$\mu$W]',
                       X_scale=1e6, X_limits=None, X_label='$10^6 X$',
                       I_scale=1e6, I_limits=None, I_label='$10^6 Q_i^{-1}$',
@@ -69,10 +84,11 @@ def plot_responsivity(df, nrows=4, ncols=4, figsize=(6, 6), loglog=False, power=
         masked = df[~df['{}_XI_fit_redchi'.format(power)].isnull()]
     else:
         masked = df[~df['{}_X'.format(power)].isnull()]
-    fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
-    # This ensures that axes is always a numpy array of AxesSubplot objects.
+    fig, X_axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=figsize)
+    # This ensures that X_axes is always a numpy array of AxesSubplot objects.
     if nrows == ncols == 1:
-        axes = np.array([axes])
+        X_axes = np.array([X_axes])
+    I_axes = np.array([X_ax.twinx() for X_ax in X_axes.flatten()]).reshape(X_axes.shape)
 
     if loglog:
         P_minimum = 10**(np.floor(np.log10(masked[power].min())) - 1)
@@ -98,13 +114,12 @@ def plot_responsivity(df, nrows=4, ncols=4, figsize=(6, 6), loglog=False, power=
             X_limits = (-10, np.ceil(X_scale * masked['{}_X'.format(power)].max() / 100) * 100)
         if I_limits is None:
             I_limits = (-1, np.ceil(I_scale * masked['{}_I'.format(power)].max() / 10) * 10)
-        #P_fit = np.linspace(P_minimum, P_maximum, 1e3)
         P_fit = np.linspace(0, P_maximum, 1e3)
 
-    for plot_index, (X_ax, (channel, group)) in enumerate(zip(axes.flatten(), masked.groupby('channel'))):
+    for plot_index, (X_ax, I_ax, (channel, group)) in enumerate(zip(X_axes.flatten(), I_axes.flatten(),
+                                                                    masked.groupby('channel'))):
         X_ax.errorbar(P_scale * group[power], X_scale * group['{}_X'.format(power)],
                       yerr=X_scale * group['{}_X_err'.format(power)], marker='.', color=X_color, linestyle='None')
-        I_ax = X_ax.twinx()
         I_ax.errorbar(P_scale * group[power], I_scale * group['{}_I'.format(power)],
                       yerr=I_scale * group['{}_I_err'.format(power)], marker='.', color=I_color, linestyle='None')
 
@@ -152,4 +167,4 @@ def plot_responsivity(df, nrows=4, ncols=4, figsize=(6, 6), loglog=False, power=
         except AttributeError:
             X_ax.set_title('{}'.format(group.channel.iloc[0]))
 
-    return fig, axes
+    return fig, X_axes, I_axes