65382ead5d778bd3d52283dec58ab0146399ccdd
gitj <glenn.caltech@gmail.com>
2016-02-21 23:40:12 -0500

    hopefully the last import error

--- kid_readout/measurement/acquire/acquire.py
+++ kid_readout/measurement/acquire/acquire.py
@@ -7,7 +7,7 @@ from kid_readout.roach import baseband
 from kid_readout.measurement.io import data_file
 from kid_readout.measurement.acquire import sweeps
 from kid_readout.analysis.resonator.resonator import Resonator
-from kid_readout.analysis.khalil import bifurcation_s21, bifurcation_guess
+from kid_readout.analysis.resonator.khalil import bifurcation_s21, bifurcation_guess
 from kid_readout.equipment import lockin_controller
 
 # TODO: move the memory management to the Roach class
