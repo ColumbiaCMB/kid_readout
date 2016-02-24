f4c317a5f5e6da356bc4d24c3a99a1f2b60bb51d
mabitbol <mabitbol15@gmail.com>
2016-02-24 11:43:53 -0500

    helper functions for new electronics board

---
+++ kid_readout/roach/attenuator.py
@@ -0,0 +1,20 @@
+import requests
+
+class Attenuator(object):
+    def __init__(self,tempip='http://192.168.1.211/'):
+        self.ip = tempip
+        self.att = self.get_att()
+
+    def get_att(self):
+        return float(self.get_query("ATT??"))
+    
+    def set_att(self, newatt):
+        if newatt > 62:
+            print "Setting attenuation too high.  Max is 62"
+        qatt = "SETATT="+str(newatt)
+        return float(self.get_query(qatt))
+    
+    def get_query(self, query):
+        query = self.ip + query
+        q = requests.get(query)
+        return q.content
