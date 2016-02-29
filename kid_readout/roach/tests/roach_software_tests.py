e867b9a3cb54b9046b72e6cb522c88f03b82ef31
gitj <glenn.caltech@gmail.com>
2016-02-29 09:18:25 -0500

    need to use mock valon when hardware not present

--- kid_readout/roach/tests/roach_software_tests.py
+++ kid_readout/roach/tests/roach_software_tests.py
@@ -4,16 +4,18 @@ __author__ = 'gjones'
 None of the tests in this module should require actual hardware
 """
 import kid_readout.roach.tests.mock_roach
+import kid_readout.roach.tests.mock_valon
 import kid_readout.roach.heterodyne
 import kid_readout.roach.baseband
 import numpy as np
 
 def test_calc_fft_bins():
     mr = kid_readout.roach.tests.mock_roach.MockRoach('roach')
+    mv = kid_readout.roach.tests.mock_valon.MockValon()
     np.random.seed(123)
     for class_ in [kid_readout.roach.heterodyne.RoachHeterodyne,
                    kid_readout.roach.baseband.RoachBaseband]:
-        ri = class_(roach=mr,initialize=False)
+        ri = class_(roach=mr,initialize=False, adc_valon=mv)
         for nsamp in 2**np.arange(10,18):
             tone_bins = np.random.random_integers(0,nsamp,size=128)
             ri.calc_fft_bins(tone_bins,nsamp)
