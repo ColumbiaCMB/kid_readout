b67ca42029bfa0648b3000fd3e38c4c0ff3efa69
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2016-03-18 17:50:24 -0400

    shortened stream length in tests

--- kid_readout/measurement/acquire/test/test_acquire.py
+++ kid_readout/measurement/acquire/test/test_acquire.py
@@ -10,6 +10,7 @@ def test_baseband_sweep():
     num_tones = 16
     num_waveforms = 2**5
     num_tone_samples = 2**10
+    length_seconds = 0.1
     ri = RoachBaseband(roach=MockRoach('roach'), initialize=False, adc_valon=MockValon())
     center_frequencies = np.linspace(100, 200, num_tones)
     offsets = np.linspace(-20e-3, 20e-3, num_waveforms)
@@ -17,15 +18,15 @@ def test_baseband_sweep():
     state = {'something': 'something state'}
 
     # Load waveforms one at a time.
-    sweep = acquire.run_sweep(ri=ri, tone_banks=tone_banks, num_tone_samples=num_tone_samples, length_seconds=0.1,
-                              state=state, description="description")
+    sweep = acquire.run_sweep(ri=ri, tone_banks=tone_banks, num_tone_samples=num_tone_samples,
+                              length_seconds=length_seconds, state=state, description="description")
     assert len(sweep.stream_arrays) == num_waveforms
     assert all([stream_array.s21_raw.shape[0] == num_tones for stream_array in sweep.stream_arrays])
 
     # Pre-load all waveforms.
     acquire.load_sweep_tones(ri, tone_banks, num_tone_samples)
-    sweep = acquire.run_sweep(ri=ri, tone_banks=tone_banks, num_tone_samples=num_tone_samples, length_seconds=1,
-                              tones_loaded=True, state=state, description="description")
+    sweep = acquire.run_sweep(ri=ri, tone_banks=tone_banks, num_tone_samples=num_tone_samples,
+                              length_seconds=length_seconds, tones_loaded=True, state=state, description="description")
     assert len(sweep.stream_arrays) == num_waveforms
     assert all([stream_array.s21_raw.shape[0] == num_tones for stream_array in sweep.stream_arrays])
 
@@ -34,6 +35,7 @@ def test_heterodyne_sweep():
     num_tones = 16
     num_waveforms = 2**5
     num_tone_samples = 2**10
+    length_seconds = 0.1
     ri = RoachHeterodyne(roach=MockRoach('roach'), initialize=False, adc_valon=MockValon())
 # TODO: test fails if this line is not commented out:
 #    ri.lo_frequency = 1000
@@ -43,14 +45,14 @@ def test_heterodyne_sweep():
     state = {'something': 'something state'}
 
     # Load waveforms one at a time.
-    sweep = acquire.run_sweep(ri=ri, tone_banks=tone_banks, num_tone_samples=num_tone_samples, length_seconds=0.1,
-                              state=state, description="description")
+    sweep = acquire.run_sweep(ri=ri, tone_banks=tone_banks, num_tone_samples=num_tone_samples,
+                              length_seconds=length_seconds, state=state, description="description")
     assert len(sweep.stream_arrays) == num_waveforms
     assert all([stream_array.s21_raw.shape[0] == num_tones for stream_array in sweep.stream_arrays])
 
     # Pre-load all waveforms.
     acquire.load_sweep_tones(ri, tone_banks, num_tone_samples)
-    sweep = acquire.run_sweep(ri=ri, tone_banks=tone_banks, num_tone_samples=num_tone_samples, length_seconds=1,
-                              tones_loaded=True, state=state, description="description")
+    sweep = acquire.run_sweep(ri=ri, tone_banks=tone_banks, num_tone_samples=num_tone_samples,
+                              length_seconds=length_seconds, tones_loaded=True, state=state, description="description")
     assert len(sweep.stream_arrays) == num_waveforms
     assert all([stream_array.s21_raw.shape[0] == num_tones for stream_array in sweep.stream_arrays])
