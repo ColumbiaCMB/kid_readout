103af13daf8d77a38c7ccf17d96bdc6fa036468d
gitj <glenn.caltech@gmail.com>
2016-04-10 13:17:03 -0400

    fix some warnings

--- kid_readout/__init__.py
+++ kid_readout/__init__.py
@@ -1,4 +1,4 @@
-from kid_readout.roach import heterodyne,baseband,r2heterodyne,r2baseband,valon
+from kid_readout.roach import heterodyne,baseband,r2heterodyne,r2baseband
 from kid_readout.roach.heterodyne import RoachHeterodyne
 from kid_readout.roach.baseband import RoachBaseband
 from kid_readout.roach.r2baseband import RoachBaseband
@@ -6,5 +6,6 @@ from kid_readout.roach.r2heterodyne import Roach2Heterodyne
 
 from kid_readout.analysis.resonator.helpers import *
 from kid_readout.measurement.io.readoutnc import ReadoutNetCDF
+from kid_readout.measurement.io.nc import IO as NCFile
 from kid_readout.analysis.resonator import lmfit_resonator
 from kid_readout.measurement.acquire import acquire
--- kid_readout/analysis/resources/local_settings.py
+++ kid_readout/analysis/resources/local_settings.py
@@ -4,6 +4,5 @@ import socket
 hostname = socket.gethostname()
 BASE_DATA_DIR = os.path.join('/data',hostname)
 if not os.path.exists(BASE_DATA_DIR):
-    print "no data directory set up for",hostname,"defaulting to /data"
     BASE_DATA_DIR = '/data'
 
