b0ef7e24199c781025788b1c1a339d355d857b30
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2016-04-18 18:00:22 -0400

    hardware now implemented using namedtuple

--- kid_readout/measurement/acquire/hardware.py
+++ kid_readout/measurement/acquire/hardware.py
@@ -1,19 +1,10 @@
+from collections import namedtuple
 from kid_readout.measurement import core
 
 
-class Hardware(dict):
+def Hardware(*args):
+    return namedtuple('Hardware', [arg.name for arg in args])(*args)
 
-    __setattr__ = dict.__setitem__
-    __getattr__ = dict.__getitem__
-    __delattr__ = dict.__delitem__
-    __getstate__ = lambda: None
-    __slots__ = ()
 
-    def __init__(self, *args):
-        super(Hardware, self).__init__([(arg.name, arg) for arg in args])
-
-    def state(self):
-        return core.StateDict([(equipment.name, equipment.state()) for equipment in self.values()])
-
-    def __repr__(self):
-        return '{}({})'.format(self.__class__.__name__, ', '.join(self.keys()))
+def state(hardware):
+    return core.StateDict([(hw.name, hw.state()) for hw in hardware])
