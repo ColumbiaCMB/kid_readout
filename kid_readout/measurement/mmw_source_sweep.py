Fixed factor of 2 in mmw_source_sweep.lockin_rms_to_zbd_voltage()
This module now attempts to create the ZBD object once per import instead of once per function call, which should be a lot faster.

kid_readout/measurement/mmw_source_sweep.py
@@ -1,22 +1,32 @@
-from kid_readout.measurement import core,basic
+from __future__ import division
+from kid_readout.measurement import core, basic
 import numpy as np
 from memoized_property import memoized_property
+# The ZBD object loads a few data files from disk. If this import fails then the functions that use it below will still
+# work, but only with default arguments.
+try:
+    from equipment.vdi.zbd import ZBD
+    zbd = ZBD()
+except ImportError:
+    zbd = None
 
 
 class MMWSweepList(basic.SweepStreamList):
 
     def __init__(self, sweep, stream_list, state, description=''):
-        super(MMWSweepList, self).__init__(sweep=sweep,stream_list=stream_list,state=state,description=description)
-    def single_sweep_stream_list(self,index):
+        super(MMWSweepList, self).__init__(sweep=sweep, stream_list=stream_list, state=state, description=description)
+
+    def single_sweep_stream_list(self, index):
         return MMWResponse(self.sweep.sweep(index),
-                                     core.MeasurementList(sa.stream(index) for sa in self.stream_list),
-                                     state=self.state, description=self.description)
+                           core.MeasurementList(sa.stream(index) for sa in self.stream_list),
+                           state=self.state, description=self.description)
 
 
 class MMWResponse(basic.SingleSweepStreamList):
-    def __init__(self, single_sweep, stream_list, state, description=''):
-        super(MMWResponse,self).__init__(single_sweep=single_sweep,stream_list=stream_list,state=state,description=description)
 
+    def __init__(self, single_sweep, stream_list, state, description=''):
+        super(MMWResponse,self).__init__(single_sweep=single_sweep, stream_list=stream_list, state=state,
+                                         description=description)
 
     @property
     def lockin_rms_voltage(self):
@@ -26,11 +36,11 @@ class MMWResponse(basic.SingleSweepStreamList):
         return zbd_voltage_to_power(self.zbd_voltage(linearize=linearize), mmw_frequency=self.mmw_frequency)
 
     def zbd_voltage(self, linearize=False):
-        return lockin_rms_to_zbd_voltage(self.lockin_rms_voltage,linearize=linearize)
+        return lockin_rms_to_zbd_voltage(self.lockin_rms_voltage, linearize=linearize)
 
     @property
     def hittite_frequency(self):
-        return np.array(self.state_vector('hittite','frequency'),dtype='float')
+        return np.array(self.state_vector('hittite','frequency'), dtype='float')
 
     @property
     def mmw_frequency(self):
@@ -40,11 +50,11 @@ class MMWResponse(basic.SingleSweepStreamList):
     def sweep_stream_list(self):
         return self.get_sweep_stream_list()
 
-    def get_sweep_stream_list(self,deglitch=False):
+    def get_sweep_stream_list(self, deglitch=False):
         result = []
         for stream in self.stream_list:
-            sss = basic.SingleSweepStream(sweep=self.sweep,stream=stream,state=stream.state,
-                                        description=stream.description)
+            sss = basic.SingleSweepStream(sweep=self.sweep, stream=stream, state=stream.state,
+                                          description=stream.description)
             sss._set_q_and_x(deglitch=deglitch)
             result.append(sss)
         return result
@@ -87,16 +97,15 @@ class MMWSweepOnMod(core.Measurement):
         
         
 def lockin_rms_to_zbd_voltage(lockin_rms_voltage, linearize=False):
-    zbd_voltage = np.sqrt(2)*np.pi*lockin_rms_voltage
+    zbd_voltage = np.pi / np.sqrt(2) * lockin_rms_voltage
     if linearize:
-        from equipment.vdi.zbd import ZBD
-        zbd_voltage = zbd_voltage / ZBD().linearity(zbd_voltage)
+        zbd_voltage /= zbd.linearity(zbd_voltage)
     return zbd_voltage
+
 
 def zbd_voltage_to_power(zbd_voltage, mmw_frequency=None):
     if mmw_frequency is None:
-        volts_per_watt = 2200.  # 2200 V/W is the approximate responsivity
+        volts_per_watt = 2200  # 2200 V/W is the approximate responsivity
     else:
-        from equipment.vdi.zbd import ZBD
-        volts_per_watt = ZBD().responsivity(mmw_frequency)
-    return zbd_voltage/volts_per_watt
+        volts_per_watt = zbd.responsivity(mmw_frequency)
+    return zbd_voltage / volts_per_watt