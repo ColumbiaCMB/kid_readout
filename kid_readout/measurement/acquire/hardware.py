09e5da5042374428ff666df4e343cdf6cde2face
gitj <glenn.caltech@gmail.com>
2016-04-19 21:38:09 -0400

    add fast state option. would be cleaner if we standardized on a fast option for all equipment, but that's less descriptive
    also check in some script changes

--- kid_readout/measurement/acquire/hardware.py
+++ kid_readout/measurement/acquire/hardware.py
@@ -7,8 +7,26 @@ class Hardware(object):
         for arg in args:
             setattr(self, arg.name, arg)
 
-    def state(self):
-        return core.StateDict([(obj.name, obj.state()) for obj in self.__dict__.values()])
+    def state(self, fast=False):
+        """
+        Get the state of all hardware
+
+        Parameters
+        ----------
+        fast: bool, if True, only get minimal data that changes frequently. For now this only applies to the lockin
+
+        Returns
+        -------
+
+        """
+        state_ = {}
+        #  this implementation might seem a bit ugly, but it works fine and is clear what is happening
+        for obj in self.__dict__.values():
+            if fast and obj.name == 'lockin':
+                state_[obj.name] = obj.state(measurement_only=True)
+            else:
+                state_[obj.name] = obj.state()
+        return core.StateDict(state_)
 
     def __repr__(self):
         return '{}({})'.format(self.__class__.__name__, ', '.join(self.__dict__.keys()))
