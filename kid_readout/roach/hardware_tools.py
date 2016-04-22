30af28816948ce181b5bc7580535b2795f4026ce
mabitbol <mabitbol15@gmail.com>
2016-04-22 10:56:31 -0400

    added valon serial numbers

--- kid_readout/roach/hardware_tools.py
+++ kid_readout/roach/hardware_tools.py
@@ -5,17 +5,20 @@ from heterodyne import RoachHeterodyne
 from r2heterodyne import Roach2Heterodyne
 from attenuator import Attenuator
 
+mark2_valon = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A101FK1H-if00-port0"
+roach2_valon = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A101FK1K-if00-port0"
+
 
 def r2_with_mk2(lo_freq=1000, atten=None):
     attenuator = Attenuator(attenuation=atten)
-    r2 = Roach2Heterodyne(roachip='r2kid', adc_valon = '/dev/ttyUSB4', lo_valon = '/dev/ttyUSB3', attenuator=attenuator)
+    r2 = Roach2Heterodyne(roachip='r2kid', adc_valon=roach2_valon, lo_valon=mark2_valon, attenuator=attenuator)
     r2.set_lo(lo_freq)
     return r2
         
    
 def r1_with_mk2(lo_freq=1000, atten=None):
     attenuator = Attenuator(attenuation=atten)
-    r1 = RoachHeterodyne(roachip='roach', adc_valon='/dev/ttyUSB0', lo_valon='/dev/ttyUSB3', attenuator=attenuator)
+    r1 = RoachHeterodyne(roachip='roach', adc_valon='/dev/ttyUSB0', lo_valon=mark2_valon, attenuator=attenuator)
     r1.initialize()
     r1.set_lo(lo_freq)
     return r1
