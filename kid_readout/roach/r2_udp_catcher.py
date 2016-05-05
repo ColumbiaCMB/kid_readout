fede6f778140d7eeb5110b70283febe8f11a5f53
mabitbol <mabitbol15@gmail.com>
2016-05-05 10:55:27 -0400

    if all packets are bad return nans

--- kid_readout/roach/r2_udp_catcher.py
+++ kid_readout/roach/r2_udp_catcher.py
@@ -54,33 +54,36 @@ def decode_packets(plist,nchans):
     num_lost = start_ind + npkts - end_ind
     plist = plist[start_ind:end_ind]
     if len(plist) == 0:
-        return np.array([]), np.array([]), npkts, 0
+        data = np.empty(chns_per_pkt, dtype='complex64')
+        data.fill(np.nan)
+        start = np.nan
+        num_bad_pkts = npkts
+        num_dropped_pkts = 0
+    else:
+        start = np.fromstring(plist[0],'<u4')[-1]
+        stop = np.fromstring(plist[-1],'<u4')[-1]
+        num_expected_pkts = (stop - start) / pkt_counter_step + 1
+        packet_counter = np.empty(num_expected_pkts)
+        packet_counter.fill(np.nan)
+        data = np.empty(num_expected_pkts*chns_per_pkt, dtype='complex64')
+        data.fill(np.nan)
 
-    start = np.fromstring(plist[0],'<u4')[-1]
-    stop = np.fromstring(plist[-1],'<u4')[-1]
+        num_bad_pkts = 0
+        for pkt in plist:
+            if len(pkt) != 1028:
+                num_bad_pkts += 1
+                continue
+            all_data = np.fromstring(pkt,'<u4')
+            pkt_addr = all_data[-1]
+            k = (pkt_addr - start) / pkt_counter_step
+            si = k * chns_per_pkt
+            sf = (k + 1) * chns_per_pkt
+            data[si:sf] = 1j*np.conj(all_data[:-1].view('<i2').astype('float32').view('complex64'))
+            packet_counter[k] = pkt_addr
 
-    num_expected_pkts = (stop - start) / pkt_counter_step + 1
-    packet_counter = np.empty(num_expected_pkts)
-    packet_counter.fill(np.nan)
-    data = np.empty(num_expected_pkts*chns_per_pkt, dtype='complex64')
-    data.fill(np.nan)
-
-    num_bad_pkts = 0
-    for pkt in plist:
-        if len(pkt) != 1028:
-            num_bad_pkts += 1
-            continue
-        all_data = np.fromstring(pkt,'<u4')
-        pkt_addr = all_data[-1]
-        k = (pkt_addr - start) / pkt_counter_step
-        si = k * chns_per_pkt
-        sf = (k + 1) * chns_per_pkt
-        data[si:sf] = 1j*np.conj(all_data[:-1].view('<i2').astype('float32').view('complex64'))
-        packet_counter[k] = pkt_addr
-
-    num_bad_pkts += num_lost
-    data = data.reshape((-1,nchans))
-    num_dropped_pkts = np.sum(np.isnan(packet_counter)) - (num_bad_pkts - num_lost)
+        num_bad_pkts += num_lost
+        data = data.reshape((-1,nchans))
+        num_dropped_pkts = np.sum(np.isnan(packet_counter)) - (num_bad_pkts - num_lost)
     return data, np.array([start]), num_bad_pkts, num_dropped_pkts
 
 def get_first_packet_index(plist):
