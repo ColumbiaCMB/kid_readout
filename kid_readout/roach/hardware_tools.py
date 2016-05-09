95f6069c95fba0163936a1acac454732c913adb5
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2016-05-09 19:58:00 -0400

    settings code ready to merge

--- kid_readout/roach/hardware_tools.py
+++ kid_readout/roach/hardware_tools.py
@@ -4,28 +4,26 @@ from interface import RoachInterface
 from heterodyne import RoachHeterodyne
 from r2heterodyne import Roach2Heterodyne
 from attenuator import Attenuator
+from kid_readout.settings import ROACH1_IP, ROACH1_VALON, ROACH2_IP, ROACH2_VALON, MARK2_VALON
 
-mark2_valon = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A101FK1H-if00-port0"
-roach2_valon = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A101FK1K-if00-port0"
-roach1_valon = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AM01H05A-if00-port0"
 
 def r2_with_mk2(lo_freq=1000, atten=None):
     attenuator = Attenuator(attenuation=atten)
-    r2 = Roach2Heterodyne(roachip='r2kid', adc_valon=roach2_valon, lo_valon=mark2_valon, attenuator=attenuator)
+    r2 = Roach2Heterodyne(roachip=ROACH2_IP, adc_valon=ROACH2_VALON, lo_valon=MARK2_VALON, attenuator=attenuator)
     r2.set_lo(lo_freq)
     return r2
         
    
 def r1_with_mk2(lo_freq=1000, atten=None):
     attenuator = Attenuator(attenuation=atten)
-    r1 = RoachHeterodyne(roachip='roach', adc_valon='/dev/ttyUSB0', lo_valon=mark2_valon, attenuator=attenuator)
+    r1 = RoachHeterodyne(roachip=ROACH1_IP, adc_valon=ROACH1_VALON, lo_valon=MARK2_VALON, attenuator=attenuator)
     r1.initialize()
     r1.set_lo(lo_freq)
     return r1
     
 
 def r1_with_mk1(lo_freq=1000):
-    r1 = RoachHeterodyne(roachip='roach', adc_valon='/dev/ttyUSB0', lo_valon=None, attenuator=None)
+    r1 = RoachHeterodyne(roachip=ROACH1_IP, adc_valon=ROACH1_VALON, lo_valon=None, attenuator=None)
     r1.initialize()
     r1.set_lo(lo_freq)
     return r1
