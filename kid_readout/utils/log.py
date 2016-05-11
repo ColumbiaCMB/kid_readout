78b7ccc83e4e598e26938d2b25124336647f0b55
gitj <glenn.caltech@gmail.com>
2016-05-11 14:22:46 -0400

    hope to fix logging error

--- kid_readout/utils/log.py
+++ kid_readout/utils/log.py
@@ -1,5 +1,5 @@
 import logging
-import logging.handlers
-default_handler = logging.handlers.StreamHandler()
+
+default_handler = logging.StreamHandler()
 default_formatter = logging.Formatter('%(levelname)s: %(asctime)s - %(name)s.%(funcName)s:%(lineno)d  %(message)s')
 default_handler.setFormatter(default_formatter)
