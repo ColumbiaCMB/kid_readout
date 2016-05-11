d4fe969a7a76618dc8f3e66bae7c1b3474f31695
gitj <glenn.caltech@gmail.com>
2016-05-11 13:46:39 -0400

    fix bad choice of name

--- kid_readout/utils/logging.py
+++
@@ -1,4 +0,0 @@
-import logging
-import logging.handlers
-default_handler = logging.handlers.StreamHandler()
-default_handler.setFormatter(logging.Formatter('%(levelname)s: %(asctime)s - %(name)s.%(funcName)s:%(lineno)d  %(message)s'))
