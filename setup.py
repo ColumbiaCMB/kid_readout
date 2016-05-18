4d8b62e89b7cb45503fde55411695acb9ec64a8d
mabitbol <mabitbol15@gmail.com>
2016-05-18 16:59:05 -0400

    moved setup for cython to correct spot

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize("decode.pyx"))
+setup(ext_modules = cythonize("kid_readout/roach/decode.pyx"))
 
