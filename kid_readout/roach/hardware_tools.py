d0bdeaf5fa4705e51420e31c8bfc1193150d46d7
gitj <glenn.caltech@gmail.com>
2016-09-12 16:08:04 -0400

    Extend check for MARK2_VALON validity

--- kid_readout/roach/hardware_tools.py
+++ kid_readout/roach/hardware_tools.py
@@ -6,6 +6,8 @@ from kid_readout.settings import *
 
 def r2_with_mk2(**kwargs):
     attenuator = Attenuator()
+    if MARK2_VALON is None:  # If lo_valon is None the roach will use its internal valon, with unfortunate results.
+        raise ValueError("MARK2_VALON is None.")
     r2 = Roach2Heterodyne(roachip=ROACH2_IP, adc_valon=ROACH2_VALON, host_ip=ROACH2_GBE_HOST_IP, lo_valon=MARK2_VALON,
                           attenuator=attenuator,**kwargs)
     return r2
