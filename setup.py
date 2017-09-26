9859707e6a5c82e075f9d611ccb5e0b81cec2c05
Daniel Flanigan <daniel.isaiah.flanigan@gmail.com>
2017-09-26 13:21:11 -0400

    setup.py possibly working and .travis.yml downloads qdr.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
 
-setup(ext_modules = cythonize("kid_readout/roach/decode.pyx"))
-
+setup(name='KID Readout',
+      description='Code for the Columbia CMB ROACH KID readout',
+      url='https://github.com/ColumbiaCMB/kid_readout',
+      packages=['kid_readout'],
+      ext_modules=cythonize('kid_readout/roach/decode.pyx'),
+      include_dirs=[numpy.get_include()])
